f4ebaeaf3472fe1d8619e33839adc03937f717fa
Toby Johnston <tobyjohnston38@gmail.com>
2019-03-25 21:53:01 +1300

    more setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <h1>Oopsie Woopsie!</h1>
+          <p>
+            Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys
+            at our headquarters are working VEWY HAWD to fix this!
+          </p>
+        </Layout>
       </div>
     );
   }
