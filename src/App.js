8d0b1759d5bf98ba37503994d1031aed573ce969
Toby Johnston <tobyjohnston38@gmail.com>
2019-03-25 22:05:41 +1300

    builder container

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from "react";
 
 import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBulider/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <h1>Oopsie Woopsie!</h1>
-          <p>
-            Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys
-            at our headquarters are working VEWY HAWD to fix this!
-          </p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
