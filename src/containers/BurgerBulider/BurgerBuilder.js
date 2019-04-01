b0ea4329b86be68437fb7d90f2a638750abdfe5b
Toby Johnston <toby@crssoftware.co.nz>
2019-04-02 11:59:42 +1300

    created order modal

--- src/containers/BurgerBulider/BurgerBuilder.js
+++ src/containers/BurgerBulider/BurgerBuilder.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
-import Aux from "../../hoc/Aux";
-import Burger from "../../components/Burger/Burger";
-import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Aux from "../../hoc/Aux"
+import Burger from "../../components/Burger/Burger"
+import BuildControls from "../../components/Burger/BuildControls/BuildControls"
+import Modal from "../../components/UI/Modal/Modal"
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
 
 const INGREDIENT_PRICES = {
   salad: 0.05,
   cheese: 0.04,
   meat: 1.3,
   bacon: 0.7
-};
+}
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -25,63 +27,66 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchaseable: false
-  };
+  }
 
   updatePurchaseState(ingredients) {
     const sum = Object.keys(ingredients)
       .map(igKey => {
-        return ingredients[igKey];
+        return ingredients[igKey]
       })
       .reduce((sum, el) => {
-        return sum + el;
-      }, 0);
-    this.setState({ purchaseable: sum > 0 });
+        return sum + el
+      }, 0)
+    this.setState({ purchaseable: sum > 0 })
   }
 
   addIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type];
-    const updatedCount = oldCount + 1;
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount + 1
     const updatedIngredients = {
       ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCount;
-    const priceAddition = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceAddition;
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchaseState(updatedIngredients);
-  };
+    }
+    updatedIngredients[type] = updatedCount
+    const priceAddition = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice + priceAddition
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+    this.updatePurchaseState(updatedIngredients)
+  }
 
   removeIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type];
+    const oldCount = this.state.ingredients[type]
     if (oldCount <= 0) {
-      return;
+      return
     }
-    const updatedCount = oldCount - 1;
+    const updatedCount = oldCount - 1
     const updatedIngredients = {
       ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCount;
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
+    }
+    updatedIngredients[type] = updatedCount
+    const priceDeduction = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceDeduction
     this.setState({
       totalPrice: newPrice,
       ingredients: updatedIngredients
-    });
-    this.updatePurchaseState();
-  };
+    })
+    this.updatePurchaseState()
+  }
 
   render() {
     const disabledInfo = {
       ...this.state.ingredients
-    };
+    }
     for (let key in disabledInfo) {
-      disabledInfo[key] = disabledInfo[key] <= 0;
+      disabledInfo[key] = disabledInfo[key] <= 0
     }
     // {salad: true, meat: false, ...}
     return (
       <Aux>
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
@@ -91,8 +96,8 @@ class BurgerBuilder extends Component {
           price={this.state.totalPrice}
         />
       </Aux>
-    );
+    )
   }
 }
 
-export default BurgerBuilder;
+export default BurgerBuilder
