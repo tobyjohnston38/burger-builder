80c166093169c5d2ffd81c0013906670ca5085ff
Toby Johnston <toby@crssoftware.co.nz>
2019-04-04 16:55:17 +1300

    working modal - broken remove

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,5 +1,10 @@
 import React from "react"
 
-const backdrop = props => props.show
+import classes from "./Backdrop.css"
+
+const backdrop = props =>
+  props.show ? (
+    <div className={classes.Backdrop} onClick={props.clicked} />
+  ) : null
 
 export default backdrop
