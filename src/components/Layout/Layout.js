84d85dbc8cb8486d1ba58445b74b6f6fd6039b2e
Toby Johnston <tobyjohnston38@gmail.com>
2019-04-23 21:14:46 +1200

    backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { Component } from "react";
 
-import Aux from "../../hoc/Aux"
-import Toolbar from "../Navigation/Toolbar/Toolbar"
-import classes from "./Layout.css"
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer"
+import Aux from "../../hoc/Aux";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import classes from "./Layout.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-)
+class Layout extends Component {
+  state = {
+    SideDrawerHandler: true
+  };
 
-export default layout
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
